43212bed0b50be68f4ef5735854d22cc0fefdead
navitas2022 <105746604+navitas2022@users.noreply.github.com>
2022-05-31 09:08:09 -0400

    Updated blob storage SAS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
     ACCOUNT_NAME: "ngangular",
-  SAS: "https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=c&sp=rwdlacupitfx&se=2022-05-16T11:23:45Z&st=2022-05-16T03:23:45Z&spr=https,http&sig=luUeF9TsxKKV1L3rgCtFlVjIO0ZjbCIFJIdi9Q%2BG%2BKo%3D",
+  SAS: "https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-06-30T20:58:58Z&st=2022-05-31T12:58:58Z&spr=https,http&sig=aoLPQaaoiSgdSqwWKqMZmOAUKtHVhLr7P8FcAlCCQ1A%3D",
   URL: "https://api-rpademo-navitas.azurewebsites.net",
 };
