15e3398d01ecf643055715d1b24ec74d6943cff9
navitas2022 <navitas20222021@gmail.com>
2022-05-31 11:36:09 -0400

    added blob SAS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ACCOUNT_NAME: "ngangular",
-  SAS: "https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-06-30T20:58:58Z&st=2022-05-31T12:58:58Z&spr=https,http&sig=aoLPQaaoiSgdSqwWKqMZmOAUKtHVhLr7P8FcAlCCQ1A%3D",
+  SAS: "https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-06-30T23:21:57Z&st=2022-05-31T15:21:57Z&sip=0.0.0.0-255.255.255.255&spr=https,http&sig=LUceFaQUGV0M66aBHjRk6%2BDYqhGNCpENHpF87%2Fj6Nco%3D",
   URL: "https://api-rpademo-navitas.azurewebsites.net",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   ACCOUNT_NAME: "ngangular",
-  SAS: "https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-05-30T20:34:19Z&st=2022-05-16T12:34:19Z&spr=https,http&sig=mYHESJQDH36jgTpJA0zGJdM3bZhjX5wT7aqPdwrAa6s%3D",
+  SAS: "https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-06-30T23:21:57Z&st=2022-05-31T15:21:57Z&sip=0.0.0.0-255.255.255.255&spr=https,http&sig=LUceFaQUGV0M66aBHjRk6%2BDYqhGNCpENHpF87%2Fj6Nco%3D",
   URL: "https://my-api-rpademo-navitas.azurewebsites.net/api"
 };
 
