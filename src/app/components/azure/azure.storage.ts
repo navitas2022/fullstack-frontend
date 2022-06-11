eb886ffd690c672c6020edb4fe4fe2634ea0485d
navitas@22 <navitas@22@gmail.com>
2022-06-11 14:10:35 -0400

    pdf opens up

--- src/app/components/azure/azure.storage.ts
+++ src/app/components/azure/azure.storage.ts
@@ -32,34 +32,34 @@ let maxNumber = 10000;
 
 
 export function setToken(token: any, container: string, currentToken: any) { // Public Domain/MIT
-  if (currentToken){
+  if (currentToken) {
     maxNumber = parseInt(currentToken);
     return;
   }
   const d = parseInt(token);
-  if (!isNaN(d)){
-    if (d >= maxNumber){
-      maxNumber = (container === AllContainers.allfiles ? d+1: d);
+  if (!isNaN(d)) {
+    if (d >= maxNumber) {
+      maxNumber = (container === AllContainers.allfiles ? d + 1 : d);
     }
   }
 }
 export function generateUUID() { // Public Domain/MIT
-    return maxNumber.toString();
+  return maxNumber.toString();
 }
 
 function UUID() { // Public Domain/MIT
   var d = new Date().getTime();//Timestamp
-  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-  return 'xxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16;//random number between 0 and 16
-      if(d > 0){//Use timestamp until depleted
-          r = (d + r)%16 | 0;
-          d = Math.floor(d/16);
-      } else {//Use microseconds since page-load if supported
-          r = (d2 + r)%16 | 0;
-          d2 = Math.floor(d2/16);
-      }
-      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16;//random number between 0 and 16
+    if (d > 0) {//Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {//Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
   });
 }
 
@@ -79,7 +79,7 @@ export async function getContainers() {
 
 export interface MyFileData {
   file: File,
-  size:string,
+  size: string,
   date: string,
   desc: string,
   token: string,
@@ -90,12 +90,12 @@ export interface MyFileData {
 
 }
 
-export function changeName(filename: string, token: string){
-  return filename.split('.')[0]+token+'.'+filename.split('.')[filename.split('.').length-1]
+export function changeName(filename: string, token: string) {
+  return filename.split('.')[0] + token + '.' + filename.split('.')[filename.split('.').length - 1]
 
 }
 
-export function newFileUpload(file: File, container: string, desc = "Description", token= '', status= 'Not started'): MyFileData{
+export function newFileUpload(file: File, container: string, desc = "Description", token = '', status = 'Not started'): MyFileData {
   token = token || generateUUID();
   let name = changeName(file.name, UUID());
   name = name.replace(/ /g, '').replace(/[^\x00-\x7F]/g, "");;
@@ -130,7 +130,7 @@ export async function listBlob(containerName: string) {
   // BlobContainerClient
   const containerClient = blobServiceClient.getContainerClient(containerName);
   let ListBlobs = [];
-  let iter = containerClient.listBlobsFlat({includeMetadata: true});
+  let iter = containerClient.listBlobsFlat({ includeMetadata: true });
   let blobItem = await iter.next();
   while (!blobItem.done) {
     ListBlobs.push(blobItem.value);
@@ -140,7 +140,7 @@ export async function listBlob(containerName: string) {
 }
 
 export async function deleteBlob(containerName: string, filename: string) {
-  console.log('deleteBlob',containerName, filename )
+  console.log('deleteBlob', containerName, filename)
 
   const containerClient = blobServiceClient.getContainerClient(containerName);
   const blockBlobClient = containerClient.getBlockBlobClient(filename);
@@ -154,7 +154,7 @@ export async function deleteContainerV(containerName: string) {
   return `Deleted Blob ${containerName} successfully ${deleteContainer.requestId}`;
 }
 
-export async function uploadFile(content: CONTENT, metadata ?: {
+export async function uploadFile(content: CONTENT, metadata?: {
   [propertyName: string]: string;
 }) {
   const containerClient = blobServiceClient.getContainerClient(content.containerName);
@@ -167,7 +167,7 @@ export async function uploadFile(content: CONTENT, metadata ?: {
   return `Upload block blob ${content.filename} successfully ${uploadBlobResponse.requestId}`;
 }
 
-export async function uploadFile2(content: MyFileData, newContainer?: string, file? :Blob, isFinished=false) {
+export async function uploadFile2(content: MyFileData, newContainer?: string, file?: Blob, isFinished = false) {
 
   const containerClient = blobServiceClient.getContainerClient(newContainer || content.container);
 
@@ -182,11 +182,23 @@ export async function uploadFile2(content: MyFileData, newContainer?: string, fi
       container: newContainer || content.container,
       desc: content.desc,
       fileName: content.fileName,
-      status: isFinished? "Finished" :content.status,
+      status: isFinished ? "Finished" : content.status,
       originalContainer: content.originalContainer
     }
   });
-  await blockBlobClient.setHTTPHeaders({blobContentDisposition: `inline;filename="${content.fileName}"`})
+
+
+
+  if (content.file.type){
+    if (content.file.type.match(/^image/) || content.file.type === 'application/pdf') {
+      await blockBlobClient.setHTTPHeaders({ blobContentDisposition: `inline;filename="${content.fileName}"` })
+    }
+  }else{
+    if ((content.file as any).properties?.contentDisposition){
+      await blockBlobClient.setHTTPHeaders({ blobContentDisposition: (content.file as any).properties?.contentDisposition })
+
+    }
+  }
 
   console.log('token', content)
 
@@ -210,7 +222,7 @@ export async function downloadBlob(containerName: string, filename: string) {
 
 
 export async function getBlob(containerName: string, filename: string) {
-  console.log('getBlob',containerName, filename )
+  console.log('getBlob', containerName, filename)
   const containerClient = blobServiceClient.getContainerClient(containerName);
   const blobClient = containerClient.getBlobClient(filename);
   // Get blob content from position 0 to the end
@@ -223,7 +235,7 @@ export async function getBlob(containerName: string, filename: string) {
 }
 
 export async function updateMetadata(containerName: string, filename: string, metadata: any) {
-  console.log('getBlob',containerName, filename )
+  console.log('getBlob', containerName, filename)
   const containerClient = blobServiceClient.getContainerClient(containerName);
   const blobClient = containerClient.getBlobClient(filename);
   await blobClient.setMetadata(metadata)
