04456d182ca5ebd54c7dd9b3578f9e81fce82d94
navitas2022 <105746604+navitas2022@users.noreply.github.com>
2022-08-25 17:29:59 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,14 @@
 export const environment = {
   production: false,
   ACCOUNT_NAME: "ngangular",
-  SAS: "https://myngangular.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-08-08T22:53:05Z&st=2022-08-08T14:53:05Z&spr=https,http&sig=7t1Jfuy%2Ff0SZSwB55G0sPO%2BiYCLqiARO9MmNvCbGEcg%3D",
+  SAS: "https://ngangular.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-04-30T05:13:40Z&st=2022-08-25T21:13:40Z&spr=https,http&sig=GEzD%2BQ5Z2gzlttbTgx4%2FnyPLH3pxeuoR2VTtPZMCv3Y%3D",//"https://myngangular.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-08-08T22:53:05Z&st=2022-08-08T14:53:05Z&spr=https,http&sig=7t1Jfuy%2Ff0SZSwB55G0sPO%2BiYCLqiARO9MmNvCbGEcg%3D",
   URL: "https://my-api-rpademo-navitas.azurewebsites.net/api"
 };
 
 /*//https://ngangular.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-05-16T11:44:51Z&st=2022-05-16T03:44:51Z&spr=https,http&sig=VW%2F0tXG2fwky6ioHMgpd%2BFz51Zs0asw0jIIRNJGyDhE%3D
- * For easier debugging in development mode, you can import the following file
+ 
+
+* For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
  * This import should be commented out in production mode because it will have a negative impact
