08e959cf852e313de9f03e8977df56ede22c276c
navitas2022 <105746604+navitas2022@users.noreply.github.com>
2022-08-25 17:29:41 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ACCOUNT_NAME: "ngangular",
-  SAS: "https://myngangular.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-08-08T22:53:05Z&st=2022-08-08T14:53:05Z&spr=https,http&sig=7t1Jfuy%2Ff0SZSwB55G0sPO%2BiYCLqiARO9MmNvCbGEcg%3D",
+  SAS: "https://ngangular.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-04-30T05:13:40Z&st=2022-08-25T21:13:40Z&spr=https,http&sig=GEzD%2BQ5Z2gzlttbTgx4%2FnyPLH3pxeuoR2VTtPZMCv3Y%3D",//"https://myngangular.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-08-08T22:53:05Z&st=2022-08-08T14:53:05Z&spr=https,http&sig=7t1Jfuy%2Ff0SZSwB55G0sPO%2BiYCLqiARO9MmNvCbGEcg%3D",
   URL: "https://api-rpademo-navitas.azurewebsites.net",
 };
